17d431a69fda8cee19be527244de281a180ef189
Stephen Skiles <stephentskiles@hotmail.com>
2024-11-06 12:48:30 -0700

    incorported gpt questions

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="spinner">
+    <div class="spinner-inner"></div>
+  </div>
+</template>
+
+<style scoped>
+.spinner {
+  border: 4px solid rgba(0, 0, 0, 0.3);
+  border-top-color: #3498db;
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  animation: spin 2s linear infinite;
+}
+
+.spinner-inner {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  border: 4px solid rgba(0, 0, 0, 0.3);
+  border-top-color: #3498db;
+  animation: spin 2s linear infinite reverse;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/views/ExamView.vue
+++ src/views/ExamView.vue
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useRoute } from "vue-router";
 import ExamQuestionCard from "@/components/ExamQuestionCard.vue";
 import ExamResult from "@/components/ExamResult.vue";
+import LoadingSpinner from "@/components/LoadingSpinner.vue";
 
 const route = useRoute();
 const currentQuestionNum = ref(1);
@@ -41,19 +42,33 @@ const maxLevelConstraint = ref();
 onMounted(async () => {
   try {
     const exam = route.params.id;
-    const response = await axios.get(`${import.meta.env.VITE_VUE_APP_API_URL}/api/Exams/${exam}`); // Can populate based upon exam info
+    // console.log(examQuestionFromApi.value, "on mount before response");
+    const response = await axios.get(
+      `${import.meta.env.VITE_VUE_APP_API_URL}/api/Exams/${exam}`
+    ); // Can populate based upon exam info
     currentCategory.value = response.data.category_1;
     allExamData.value = response.data;
     numOfQuestions.value = response.data.number_of_questions;
+    const divisor = numOfQuestions.value / 4;
+    maxLevelConstraint.value = divisor;
+
     const response2 = await axios.get(
-      `${import.meta.env.VITE_VUE_APP_API_URL}/api/Questions/${exam}/1/${response.data.category_1}`
+      // `${import.meta.env.VITE_VUE_APP_API_URL}/api/Questions/${exam}/1/${response.data.category_1}`
+      `${import.meta.env.VITE_VUE_APP_API_URL}/api/Ai/1/${
+        maxLevelConstraint.value
+      }/${response.data.category_1}`
     ); //This is the initial starting question
 
-    examQuestionFromApi.value = response2.data;
+    // difference between this and db fetched questions is that response2.data can be used directly w/o parsing
+    let parsedGptQuestion = parseGptResponse(response2.data);
 
-    setExamAnswerOptions(response2.data);
+    examQuestionFromApi.value = parsedGptQuestion;
+  
+    setExamAnswerOptions(parsedGptQuestion);
 
     shuffle(examAnswerOptions.value);
+
+    // console.log(examQuestionFromApi.value, "after mount after response");
 
     // This is used for populating the data for my category stats
     const allCategories = [
@@ -69,8 +84,6 @@ onMounted(async () => {
     }
 
     // Populating the queue of categories
-    const divisor = numOfQuestions.value / 4;
-    maxLevelConstraint.value = divisor;
     for (let i = 0; i < allCategories.length; i++) {
       for (let j = 0; j < divisor; j++) {
         categoryQueue.value.push(allCategories[i]);
@@ -91,30 +104,40 @@ onMounted(async () => {
 // When currentQuestionNum changes, get new question based upon level
 watch(currentQuestionNum, async () => {
   if (currentQuestionNum.value <= numOfQuestions.value) {
-
     // getting top category from the queue, basically random category
     currentCategory.value = categoryQueue.value.pop();
-    
+    // console.log(examQuestionFromApi.value, "on watch before response");
+
     // matching up our random category to a level value to know which level to get along with which category
     let level;
     const indexOfCurrentCategoryInCategoryStats = categoryData.value.findIndex(
       (obj) => obj.categoryName === currentCategory.value
     );
     if (indexOfCurrentCategoryInCategoryStats !== -1) {
-      const categoryObject = categoryData.value[indexOfCurrentCategoryInCategoryStats];
+      const categoryObject =
+        categoryData.value[indexOfCurrentCategoryInCategoryStats];
       level = categoryObject.categoryLevel;
     }
 
     const exam = route.params.id;
     const response2 = await axios.get(
-      `${import.meta.env.VITE_VUE_APP_API_URL}/api/Questions/${exam}/${level}/${currentCategory.value}`
-    ); 
-    //This is the initial starting question
-    examQuestionFromApi.value = response2.data;
+      // `${import.meta.env.VITE_VUE_APP_API_URL}/api/Questions/${exam}/${level}/${
+      //   currentCategory.value
+      // }`
+      `${import.meta.env.VITE_VUE_APP_API_URL}/api/Ai/${level}/${
+        maxLevelConstraint.value
+      }/${currentCategory.value}`
+    );
 
+    // difference between this and db fetched questions is that response2.data can be used directly w/o parsing
+    let parsedGptQuestion = parseGptResponse(response2.data);
+
+    examQuestionFromApi.value = parsedGptQuestion;
+
+    // console.log(examQuestionFromApi.value, "on watched after set");
+  
     // Populate answer options and then shuffle them
-    setExamAnswerOptions(response2.data);
-
+    setExamAnswerOptions(parsedGptQuestion);
     shuffle(examAnswerOptions.value);
   }
 });
@@ -148,6 +171,16 @@ function shuffle(array) {
   }
 }
 
+function parseGptResponse(text){
+    let jsonStartIndex = text.indexOf("{");
+    let jsonEndIndex = text.lastIndexOf("}");
+    let extractedJsonString = text.substring(
+      jsonStartIndex,
+      jsonEndIndex + 1
+    );
+    return JSON.parse(extractedJsonString);
+}
+
 const handleAnswerSubmission = (e) => {
   e.preventDefault();
   // With lower and upper limits of questionArray in mind, increase or decrease level
@@ -159,7 +192,8 @@ const handleAnswerSubmission = (e) => {
     (obj) => obj.categoryName === currentCategory.value
   );
   if (indexOfCurrentCategoryInCategoryStats !== -1) {
-    const categoryObject = categoryData.value[indexOfCurrentCategoryInCategoryStats];
+    const categoryObject =
+      categoryData.value[indexOfCurrentCategoryInCategoryStats];
     if (selectedAnswer.value === "correct") {
       if (categoryObject.categoryLevel < maxLevelConstraint.value) {
         categoryObject.categoryLevel++;
@@ -168,7 +202,7 @@ const handleAnswerSubmission = (e) => {
     } else {
       if (categoryObject.categoryLevel > 1) {
         categoryObject.categoryLevel--;
-      } 
+      }
       if (categoryObject.categoryScore > 0) {
         categoryObject.categoryScore--;
       }
@@ -176,6 +210,8 @@ const handleAnswerSubmission = (e) => {
   }
 
   selectedAnswer.value = "";
+  // For knowing when new question ic loading in
+  examQuestionFromApi.value = undefined;
   currentQuestionNum.value++;
 };
 
@@ -192,8 +228,9 @@ function handleSelectedAnswerChange(newAnswer) {
     >
       <!-- Most of the below reactive values are fed in from from the watch hook that triggiers from the handleAnswerSubmission event and the function itself -->
       <!-- : is a shortcut for v-bind -->
+      <LoadingSpinner v-if="!examQuestionFromApi && currentQuestionNum <= numOfQuestions"/>
       <ExamQuestionCard
-        v-if="currentQuestionNum <= numOfQuestions"
+        v-else-if="currentQuestionNum <= numOfQuestions"
         :current-question-num="currentQuestionNum"
         :num-of-questions="numOfQuestions"
         :exam-question-from-api="examQuestionFromApi"
@@ -203,15 +240,14 @@ function handleSelectedAnswerChange(newAnswer) {
         @update:selectedAnswer="handleSelectedAnswerChange"
       />
       <ExamResult
-        v-else
+        v-else-if="currentQuestionNum > numOfQuestions"
         :categoryData="categoryData"
         :allExamData="allExamData"
       />
     </div>
 
     <!-- For development -->
-    <!-- <p class="text-white paragraph-font ml-5">{{ selectedAnswer }}</p>
-    <p class="text-white paragraph-font ml-5">{{ categoryData }}</p> -->
-
+    <p class="text-white paragraph-font ml-5">{{ selectedAnswer }}</p>
+    <p class="text-white paragraph-font ml-5">{{ categoryData }}</p>
   </main>
 </template>
